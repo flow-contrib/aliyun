3fa8ea712e128629334cd631b4669748c8935de8
Zeal <xujinzheng@gmail.com>
2018-04-28 09:53:16 +0800

    migrate to offical package os oss

--- aliyun_oss.go
+++ aliyun_oss.go
@@ -1,28 +1,31 @@
 package aliyun
 
 import (
-	"github.com/denverdino/aliyungo/oss"
+	"fmt"
+	"github.com/aliyun/aliyun-oss-go-sdk/oss"
+	"github.com/sirupsen/logrus"
+	"strings"
 )
 
 type OSSBucketCreateArgs struct {
 	Name string
-	Perm oss.ACL
+	Perm string
 }
 
-func (p *Aliyun) CreateOSSBucketArgs() (createArgs []*OSSBucketCreateArgs, err error) {
+func (p *Aliyun) CreateOSSBucket() (err error) {
 
-	resp, err := p.OSSClient().GetService()
+	resp, err := p.OSSClient().ListBuckets()
 
 	if err != nil {
 		return
 	}
 
-	mapBuckets := map[string]*oss.BucketInfo{}
+	mapBuckets := map[string]*oss.BucketProperties{}
 	for i, bucketInfo := range resp.Buckets {
 		mapBuckets[bucketInfo.Name] = &resp.Buckets[i]
 	}
 
-	ossConf := p.Config.GetConfig("aliyun.oss")
+	ossConf := p.Config.GetConfig("aliyun.oss.bucket")
 
 	var args []*OSSBucketCreateArgs
 	for _, key := range ossConf.Keys() {
@@ -37,30 +40,48 @@ func (p *Aliyun) CreateOSSBucketArgs() (createArgs []*OSSBucketCreateArgs, err e
 
 		arg := &OSSBucketCreateArgs{
 			Name: bucketName,
-			Perm: oss.ACL(ossConf.GetString(bucketName+".perm", "private")),
+			Perm: ossConf.GetString(bucketName+".perm", "private"),
 		}
 
 		args = append(args, arg)
 	}
 
-	createArgs = args
+	for _, arg := range args {
+		err = p.OSSClient().CreateBucket(arg.Name)
+		if err != nil {
+			return
+		}
+
+		logrus.WithField("code", p.Code).WithField("bucket", arg.Name).Infoln("bucket created")
+	}
 
 	return
 }
 
-func (p *Aliyun) OSSBuckets() []*oss.Bucket {
+func (p *Aliyun) DeleteOSSBucket() (err error) {
 
-	ossConf := p.Config.GetConfig("aliyun.oss")
+	ossConf := p.Config.GetConfig("aliyun.oss.bucket")
 
-	var buckets []*oss.Bucket
 	for _, key := range ossConf.Keys() {
 
 		bucketName := ossConf.GetString(key+".name", key)
 
-		bucket := p.OSSClient().Bucket(bucketName)
+		err = p.OSSClient().DeleteBucket(bucketName)
 
-		buckets = append(buckets, bucket)
+		if err != nil {
+
+			if strings.Contains(err.Error(), "ErrorCode=NoSuchBucket") {
+				logrus.WithField("code", p.Code).WithField("bucket", bucketName).Debugln("bucket not exist, ignore to delete")
+				err = nil
+				continue
+			}
+
+			err = fmt.Errorf("delete bucket '%s' failure: %s", bucketName, err.Error())
+			return
+		}
+
+		logrus.WithField("code", p.Code).WithField("bucket", bucketName).Infoln("bucket deleted")
 	}
 
-	return buckets
+	return
 }
--- oss.go
+++ oss.go
@@ -1,12 +1,9 @@
 package aliyun
 
 import (
-	"strings"
-
 	"github.com/gogap/config"
 	"github.com/gogap/context"
 	"github.com/gogap/flow"
-	"github.com/sirupsen/logrus"
 )
 
 func init() {
@@ -18,19 +15,10 @@ func CreateOSSBucket(ctx context.Context, conf config.Configuration) (err error)
 
 	aliyun := NewAliyun(ctx, conf)
 
-	args, err := aliyun.CreateOSSBucketArgs()
+	err = aliyun.CreateOSSBucket()
 
 	if err != nil {
 		return
-	}
-
-	for _, arg := range args {
-
-		bucket := aliyun.OSSClient().Bucket(arg.Name)
-		err = bucket.PutBucket(arg.Perm)
-		if err != nil {
-			return
-		}
 	}
 
 	return
@@ -40,24 +28,10 @@ func DeleteOSSBucket(ctx context.Context, conf config.Configuration) (err error)
 
 	aliyun := NewAliyun(ctx, conf)
 
-	buckets := aliyun.OSSBuckets()
+	err = aliyun.DeleteOSSBucket()
 
-	for _, bucket := range buckets {
-		e := bucket.DelBucket()
-
-		if e != nil {
-			if strings.Contains(e.Error(), "NoSuchBucket") {
-				continue
-			}
-
-			err = e
-			return
-		}
-
-		logrus.WithField("CODE", aliyun.Code).
-			WithField("OSS-BUCKET", bucket.Name).
-			WithField("OSS-BUCKET-REGION", bucket.Region).
-			Infoln("Bucket deleted")
+	if err != nil {
+		return
 	}
 
 	return
